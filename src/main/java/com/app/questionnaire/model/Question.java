bc39b9b6b5f27f1075cc6121e72eb0035154e403
DJAngel973 <angel.djuancho@gmail.com>
2026-01-03 07:28:51 -0500

    feat: add attributes question class

--- src/main/java/com/app/questionnaire/model/Question.java
+++ src/main/java/com/app/questionnaire/model/Question.java
@@ -1,4 +1,9 @@
 package com.app.questionnaire.model;
 
 public class Question {
+    private int id;
+    private String questionText;
+    private String options;
+    private String correctAnswer;
+
 }

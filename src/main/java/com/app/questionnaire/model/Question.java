580259e39849650f8ac48cee1a6209493e494211
DJAngel973 <angel.djuancho@gmail.com>
2026-01-04 18:44:36 -0500

    feat: add constructor in question

--- src/main/java/com/app/questionnaire/model/Question.java
+++ src/main/java/com/app/questionnaire/model/Question.java
@@ -1,11 +1,23 @@
 package com.app.questionnaire.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Question {
     private int id;
     private String questionText;
-    private ArrayList<String> options;
+    private List<String> options;
     private String correctAnswer;
 
+    public Question(int id, String questionText, List<String> options, String correctAnswer) {
+        this.id = id;
+        this.questionText = questionText;
+        this.options = new ArrayList<>();
+        this.correctAnswer = correctAnswer;
+    }
+
+    public Question() {
+        this.options = new ArrayList<>();
+    }
+    
 }

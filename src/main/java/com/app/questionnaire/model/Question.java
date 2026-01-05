84e4e66e8bb8df93c878dc648c4b19dee8184285
DJAngel973 <angel.djuancho@gmail.com>
2026-01-04 19:08:07 -0500

    feat: add getter and setters

--- src/main/java/com/app/questionnaire/model/Question.java
+++ src/main/java/com/app/questionnaire/model/Question.java
@@ -12,12 +12,39 @@ public class Question {
     public Question(int id, String questionText, List<String> options, String correctAnswer) {
         this.id = id;
         this.questionText = questionText;
-        this.options = new ArrayList<>();
+        this.options = new ArrayList<>(options);
         this.correctAnswer = correctAnswer;
     }
 
     public Question() {
         this.options = new ArrayList<>();
     }
-    
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+    public void setOptions(List<String> options) {
+        this.options = new ArrayList<>(options);
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
 }

3eb3abb405f465bf3a89980df37b840afe93db8c
DJAngel973 <angel.djuancho@gmail.com>
2026-01-04 22:51:54 -0500

    feat: add loadUserByUsername method

--- src/main/java/com/app/questionnaire/service/QuizUserDetailsService.java
+++ src/main/java/com/app/questionnaire/service/QuizUserDetailsService.java
@@ -1,4 +1,33 @@
 package com.app.questionnaire.service;
 
-public class QuizUserDetailsService {
+import com.app.questionnaire.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.core.userdetails.User.builder;
+
+@Service
+public class QuizUserDetailsService implements UserDetailsService {
+
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = users.stream()
+                .filter(us -> us.getUsername().equals(username))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("User not find."));
+
+        return builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
 }

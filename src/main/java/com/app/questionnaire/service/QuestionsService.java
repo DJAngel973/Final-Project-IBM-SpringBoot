3037392e9d21ce68215926bdf36c29e826a399a1
DJAngel973 <angel.djuancho@gmail.com>
2026-01-05 21:47:05 -0500

    feat: add method loadQuizzes

--- src/main/java/com/app/questionnaire/service/QuestionsService.java
+++ src/main/java/com/app/questionnaire/service/QuestionsService.java
@@ -1,4 +1,15 @@
 package com.app.questionnaire.service;
 
+import com.app.questionnaire.model.Question;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 public class QuestionsService {
+
+    private Map<Integer, Question> questions = new HashMap<>();
+
+    public List<Question> loadQuizzes() {
+        return questions.values().stream().toList();
+    }
 }

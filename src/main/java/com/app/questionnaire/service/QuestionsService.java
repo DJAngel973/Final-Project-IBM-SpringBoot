34e20beb89df6c845ff21afc0ab5a1ef19a3b879
DJAngel973 <angel.djuancho@gmail.com>
2026-01-05 21:48:46 -0500

    feat: add annotation Service

--- src/main/java/com/app/questionnaire/service/QuestionsService.java
+++ src/main/java/com/app/questionnaire/service/QuestionsService.java
@@ -1,10 +1,13 @@
 package com.app.questionnaire.service;
 
 import com.app.questionnaire.model.Question;
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
+@Service
 public class QuestionsService {
 
     private Map<Integer, Question> questions = new HashMap<>();

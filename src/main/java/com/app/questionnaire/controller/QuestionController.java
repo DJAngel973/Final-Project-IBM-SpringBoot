b6c42e747be1d1191b4f78b213e6adeb0c0a1a28
DJAngel973 <angel.djuancho@gmail.com>
2026-01-07 20:51:55 -0500

    feat: add endpoint get login

--- src/main/java/com/app/questionnaire/controller/QuestionController.java
+++ src/main/java/com/app/questionnaire/controller/QuestionController.java
@@ -1,11 +1,16 @@
 package com.app.questionnaire.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/")
 public class QuestionController {
 
+    @GetMapping("/login")
+    public String showLogin() {
+        return "login";
+    }
 
 }

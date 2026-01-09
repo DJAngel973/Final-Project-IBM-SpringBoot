876dbe710239783f9513b6cda62aa4d2a2af1710
DJAngel973 <angel.djuancho@gmail.com>
2026-01-08 23:42:27 -0500

    feat: add endpoint post register user

--- src/main/java/com/app/questionnaire/controller/QuestionController.java
+++ src/main/java/com/app/questionnaire/controller/QuestionController.java
@@ -1,14 +1,20 @@
 package com.app.questionnaire.controller;
 
+import com.app.questionnaire.service.QuizUserDetailsService;
 import org.springframework.ui.Model;
 import com.app.questionnaire.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
 public class QuestionController {
+
+    private final QuizUserDetailsService userDetailsService;
+
+    public QuestionController(QuizUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     // Get para recuperar la pagina de inicio de sesión.
     @GetMapping("/login")
@@ -21,5 +27,22 @@ public class QuestionController {
     public String showRegister(Model model) {
         model.addAttribute("user", new User());
         return "register";
+    }
+
+    // Post para registrar usuario
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user, Model model) {
+        try {
+            userDetailsService.registerUsers(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getEmail(),
+                    "USER"
+            );
+            return "redirect:/login";
+        } catch (IllegalArgumentException error) {
+            model.addAttribute("error", "The user already exists");
+            return "register";
+        }
     }
 }
